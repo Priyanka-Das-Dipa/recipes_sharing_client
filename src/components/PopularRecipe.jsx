422f0b169206f0a7321617da1da70e93f7cc41bf
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-27 23:15:15 +0600

    working

--- src/components/PopularRecipe.jsx
+++ src/components/PopularRecipe.jsx
@@ -1,16 +1,17 @@
 import { FiArrowUpRight } from "react-icons/fi";
 const PopularRecipe = () => {
-    return (
-        <section className="my-28 ">
-            <div>
-                <h2>Popular Recipes</h2>
-                <button className="flex gap-5 items-center">View more <FiArrowUpRight /></button>
-            </div>
-        <div>
-            
-        </div>
-        </section>
-    );
+  return (
+    <section className="my-28 ">
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">Popular Recipes</h2>
+        <button className="flex gap-2 items-center justify-center bg-primary py-2 px-3 rounded-md text-white">
+          View more <FiArrowUpRight />
+        </button>
+      </div>
+      <hr className="mt-3" />
+      <div></div>
+    </section>
+  );
 };
 
 export default PopularRecipe;
