bef114cff8327ec88f5a7ec782d069cbc697057f
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-30 20:03:58 +0600

    contribution section done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../../components/Banner";
 import Flovers from "../../components/Flovers";
 import PopularCategory from "../../components/PopularCategory";
 import PopularRecipe from "../../components/PopularRecipe/PopularRecipe";
+import Contributor from "../../components/contributor/Contributor";
 import Update from "../../components/update/Update";
 
 
@@ -13,6 +14,7 @@ const Home = () => {
       <PopularRecipe></PopularRecipe>
       <PopularCategory></PopularCategory>
       <Update></Update>
+      <Contributor></Contributor>
     </div>
   );
 };
