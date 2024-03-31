f6c854a32aadd52828a61e7c977c01cd14ddd45c
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-31 23:36:07 +0600

    working

--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -1,14 +1,14 @@
-// import chef from "../../../public/images/chef.jpg";
+
 import "./Update.css"
 const Update = () => {
   return (
     <div className="relative">
-      {/* <img
-        src={chef}
-        alt=""
-        className="absolute inset-0 w-full h-full bg-no-repeat bg-cover"
-      /> */}
       <div className="absolute inset-0 flex flex-col items-center image-container justify-center">
+        <div className="space-y-1 mb-3">
+
+        <h1 className="text-5xl font-bold text-white">Get update delicious recipes everyday!</h1>
+        <p>Subscribe now to receive our daily newsletter filled with mouthwatering recipes, cooking tips, and culinary inspiration straight to your inbox. Whether you are a cooking enthusiast or just looking for new ideas, our curated selection of delicious recipes will delight your taste buds and elevate your culinary adventures.</p>
+        </div>
         <form className="bg-white p-8 rounded-md shadow-md">
           <div className="flex gap-5">
             <div>
